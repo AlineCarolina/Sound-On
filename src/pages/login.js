ceeac1bebcdf691c8cfafce0b1ff9e4b636d523d
AlineCarolina <eualine2017@gmail.com>
2021-09-15 17:19:32 -0300

    botão - requisito 2

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.validaInput = this.validaInput.bind(this);
+    this.botaoEnviar = this.botaoEnviar.bind(this);
+
+    this.state = {
+      textoInput: '',
+    };
+  }
+
+  validaInput(event) {
+    this.setState({
+      textoInput: event.target.value,
+    });
+    const { textoInput } = this.state;
+    const caracteres = 2;
+    if (textoInput.length === caracteres) {
+      document.getElementById('submit').disabled = false;
+    }
+  }
+
+  botaoEnviar() {
+    createUser();
+  }
+
   render() {
+    const { textoInput } = this.state;
     return (
       <div data-testid="page-login">
-        page-login
+        <form>
+          <label htmlFor="login">
+            Nome
+            <input
+              data-testid="login-name-input"
+              type="text"
+              value={ textoInput }
+              onChange={ this.validaInput }
+              name="textoInput"
+            />
+            <button
+              data-testid="login-submit-button"
+              type="submit"
+              onClick={ createUser }
+              disabled
+              id="submit"
+            >
+              Entrar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }

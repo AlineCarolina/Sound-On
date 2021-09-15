2072897c9e71c903d4cabbdea513cbe7c85679ee
AlineCarolina <eualine2017@gmail.com>
2021-09-15 20:13:20 -0300

    requisito 2

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
+import Loading from './pageLoading';
 
 class Login extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.validaInput = this.validaInput.bind(this);
     this.botaoEnviar = this.botaoEnviar.bind(this);
 
     this.state = {
       textoInput: '',
+      loading: false,
+      redirect: false,
     };
   }
 
@@ -24,13 +28,23 @@ class Login extends React.Component {
     }
   }
 
-  botaoEnviar() {
+  async botaoEnviar() {
     const { textoInput } = this.state;
-    createUser({ name: textoInput });
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name: textoInput });
+    this.setState({
+      loading: false,
+      redirect: true,
+    });
   }
 
   render() {
-    const { textoInput } = this.state;
+    const { textoInput, loading, redirect } = this.state;
+    if (loading) return <Loading />;
+    if (redirect) return <Redirect to="/search" />;
+
     return (
       <div data-testid="page-login">
         <form onSubmit={ this.botaoEnviar }>

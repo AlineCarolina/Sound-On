001246721cb8929f750226fd90ef30438e97bca1
AlineCarolina <eualine2017@gmail.com>
2021-09-16 13:59:18 -0300

    requisito 3

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,11 +1,41 @@
 import React from 'react';
-// import { getUser }  from '../services/userAPI';
+import { getUser } from '../services/userAPI';
+import Loading from './pageLoading';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.renderizaLoad();
+  }
+
+  async renderizaLoad() {
+    const user = await getUser();
+    if (user) {
+      this.setState({
+        user,
+        loading: false,
+      });
+    }
+  }
+
   render() {
+    const { user, loading } = this.state;
+
+    if (loading) return <Loading />;
+
     return (
       <header data-testid="header-component">
-        header
+        <section data-testid="header-user-name">
+          { user.name }
+        </section>
       </header>
     );
   }

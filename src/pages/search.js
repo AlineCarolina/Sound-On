430194c82d04a88d0720991a89d8bd7fc8e82c83
AlineCarolina <eualine2017@gmail.com>
2021-09-16 18:00:34 -0300

    requisito 5

--- src/pages/search.js
+++ src/pages/search.js
@@ -2,10 +2,46 @@ import React from 'react';
 import Header from './header';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+    };
+
+    this.validaInput = this.validaInput.bind(this);
+  }
+
+  validaInput(event) {
+    this.setState({
+      inputText: event.target.value,
+    });
+    const { inputText } = this.state;
+    const caracteres = 1;
+    if (inputText.length === caracteres) {
+      document.getElementById('submit').disabled = false;
+    }
+  }
+
   render() {
+    const { inputText } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            value={ inputText }
+            onChange={ this.validaInput }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled
+            id="submit"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }

b979fafa223a52ac311a66065e20caae440d91f3
Aline <eualine2017@gmail.com>
2022-06-15 17:27:06 -0300

    fix: bookmarks page working

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Header from '../components/Header';
+import Loading from './PageLoading';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import MusicCard from './MusicCard';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      favorite: [],
+    };
+  }
+
+  componentDidUpdate() {
+    getFavoriteSongs().then((favoriteSongs) => {
+      const favorite = favoriteSongs.slice(1);
+      this.setState({ favorite });
+    });
+  }
+
   render() {
+    const { loading, favorite } = this.state;
+    if (loading) { return <Loading />; }
     return (
       <div id="page-favorites">
         <Header />
+        <MusicCard songs={ favorite } />
       </div>
     );
   }
